8e0144d740592a6b730b71bd30f9a570096e7546
MoeRon2 <fbml421@gmail.com>
2025-09-16 14:04:59 +0300

    Change from individual event listeners to row event listeners for performance

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ function createGrid(size) {
         const newRow = document.createElement("div");
         newRow.style.display = "flex";
         newRow.style.flex = "1";
+        newRow.addEventListener("mouseover", function (event) {
+            let target = event.target;
+            target.style.backgroundColor = "green";
+        });
         for (let j = 0; j < size; j++) {
             const newDiv = document.createElement("div");
             newDiv.style.border = "solid";
             newDiv.style.flex = "1";
-            newDiv.addEventListener("mouseover", () => newDiv.style.color = "blue");
+            // newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "blue");
             newRow.appendChild(newDiv);
         }
         container.appendChild(newRow);
     }
 }
+
+
+
 
 
 createGrid(16);
